313ca6ae75974617223aaf37c3302c02e06600bb
Uttsav Pokhrel <uttsavpokhrel@gmail.com>
2021-11-21 15:40:09 +0545

    added a button and a score count

--- rps-game.js
+++ rps-game.js
@@ -1,15 +1,66 @@
+const moves=["rock","paper","scissor"];
+let mlen= moves.length;
+
 function computerPlay() {
-    // random rps 
-    let moves=['Rock','Paper','Scissor'];
-    let move = Math.floor(Math.random()*moves.length);
-    return moves[move];
+    return moves[Math.floor(Math.random()*mlen)];
 }
+
+const computerSelection= computerPlay(moves);
+const playerSelection= "rock";
+console.log(computerSelection);
+
+let result;
+let win="Oh! You? won?";
+let loss="I won!";
+let draw="Haha!You got lucky.";
+let c=0;
+let p=0;
 
 function score(playerSelection,computerSelection) {
-    // declare win or loss
+    // declare win or loss and add points
+
+    if (computerSelection=="rock") {
+        result=draw;
+        c=c+0.5;
+        p=p+0.5;
+    }
+    else if(computerSelection=="scissor"){
+        result=loss;
+        p=p+1;
+    }
+    else{
+        result=win;
+        c=c+1;
+    }
+    
+    return result;
 }
 
-const playerSelection= "Rock";
-const computerSelection= computerPlay();
-
 console.log(score(playerSelection,computerSelection));
+
+// let p=0;
+// let c=0;
+// function points(p,c){
+//     if (result=="win"){
+//         c=c+1;
+//     }
+
+//     else if (result=="loss"){
+//         p=p+1;
+//     }
+
+//     else{
+//         c=c+0.5;
+//         p=p+0.5;
+//     }
+
+//     return c,p;
+// }
+
+// pointsystem=points(c,p);
+console.log("Computer:"+c);
+console.log("Player:"+p);
+
+
+
+
