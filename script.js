59306034f66f8c809b9b1d07465e3217ada42cfe
Nihal upreti <n.ytuchiha@gmail.com>
2022-10-19 20:12:31 +0545

    enabled autoclick on choices.

--- script.js
+++ script.js
@@ -1,102 +1,109 @@
 let result;
 let fscore;
 
-let win="Oh! You? won?";
-let loss="I won!";
-let draw="Haha!You got lucky.";
-let c=0;
-let p=0;
-let i=0;
-Yourname=prompt("Enter your name:");
+let win = "Oh! You? won?";
+let loss = "I won!";
+let draw = "Haha!You got lucky.";
+let c = 0;
+let p = 0;
+let i = 0;
+let playerSelection = "rock"; //default is rock
+const startButton = document.getElementById("playagain");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissor = document.getElementById("scissor");
+Yourname = prompt("Enter your name:");
 
-window.onload= function(){
-    document.getElementById('playagain').onclick=function() {
+const autoStart = () => {
+  startButton.click();
+};
 
-const moves=["rock","paper","scissor"];
-let mlen= moves.length;
+document.getElementById("playagain").onclick = function () {
+  const moves = ["rock", "paper", "scissor"];
+  let mlen = moves.length;
 
-function computerPlay() {
-    return moves[Math.floor(Math.random()*mlen)];
-}
+  function computerPlay() {
+    return moves[Math.floor(Math.random() * mlen)];
+  }
 
-const computerSelection= computerPlay(moves);
+  const computerSelection = computerPlay(moves);
 
-rock.addEventListener('click',function(){
-    playerSelection="rock"
-});
+  // rock.addEventListener("click", function () {
+  //   playerSelection = "rock";
+  // });
 
-paper.addEventListener('click',function(){
-    playerSelection="paper"
-});
+  rock.addEventListener("click", () => {
+    playerSelection = "rock";
+    autoStart();
+  });
 
-scissor.addEventListener('click',function(){
-    playerSelection="scissor"
-});
+  paper.addEventListener("click", () => {
+    playerSelection = "paper";
+    autoStart();
+  });
 
-// playerSelection=prompt("Enter your move in lowercaps:");
+  scissor.addEventListener("click", () => {
+    playerSelection = "scissor";
+    autoStart();
+  });
 
-console.log(computerSelection);
+  // playerSelection=prompt("Enter your move in lowercaps:");
 
-document.getElementById("playerselection").innerHTML =playerSelection.toUpperCase();
-document.getElementById("computerselection").innerHTML =computerSelection.toUpperCase();
+  console.log(computerSelection);
 
+  document.getElementById("playerselection").innerHTML =
+    playerSelection.toUpperCase();
+  document.getElementById("computerselection").innerHTML =
+    computerSelection.toUpperCase();
 
-function score(playerSelection,computerSelection) {
-    
-    if (computerSelection=="rock" && playerSelection=="paper"|| computerSelection=="paper" && playerSelection=="scissor" || computerSelection=="scissor" && playerSelection=="rock") {
-        result=win;
-        p=p+1;
+  function score(playerSelection, computerSelection) {
+    if (
+      (computerSelection == "rock" && playerSelection == "paper") ||
+      (computerSelection == "paper" && playerSelection == "scissor") ||
+      (computerSelection == "scissor" && playerSelection == "rock")
+    ) {
+      result = win;
+      p = p + 1;
+    } else if (computerSelection == playerSelection) {
+      result = draw;
+      p = p + 0.5;
+      c = c + 0.5;
+    } else {
+      result = loss;
+      c = c + 1;
     }
-    else if(computerSelection==playerSelection){
-        result=draw;
-        p=p+0.5;
-        c=c+0.5;
-    }
-    else{
-        result=loss;
-        c=c+1;
-    }
-    
+
     return result;
-}
+  }
 
-i=i+1;
+  i = i + 1;
 
-console.log(score(playerSelection,computerSelection));
-document.getElementById("computerscore").innerHTML = c;
-document.getElementById("playerscore").innerHTML = p;
-document.getElementById("playername").innerHTML =Yourname.toUpperCase();
-document.getElementById("computer").innerHTML = "COMPUTER";
-console.log("Computer:"+c);
-console.log("Player:"+p);
+  console.log(score(playerSelection, computerSelection));
+  document.getElementById("computerscore").innerHTML = c;
+  document.getElementById("playerscore").innerHTML = p;
+  document.getElementById("playername").innerHTML = Yourname.toUpperCase();
+  document.getElementById("computer").innerHTML = "COMPUTER";
+  console.log("Computer:" + c);
+  console.log("Player:" + p);
 
-let fscore;
+  let fscore;
 
-while(i>=5){
-
-function finalscore(c,p) {
-
-    if (c>p){
-        fscore=" Computer won! "
+  while (i >= 5) {
+    function finalscore(c, p) {
+      if (c > p) {
+        fscore = " Computer won! ";
+      } else if (c == p) {
+        fscore = " It's a draw! ";
+      } else {
+        fscore = " " + Yourname + " " + "won! ";
+      }
+      return fscore;
     }
-    else if(c==p){
-        fscore=" It's a draw! "
-    }
 
-    else{
-        fscore=" "+Yourname+" "+"won! "
-    }
-    return fscore;
-}
-
-
-console.log(finalscore(c,p));
-document.getElementById("message").innerHTML =fscore;
-i=0;
-c=0;
-p=0;
-    }
-    
-}
-
-}
+    console.log(finalscore(c, p));
+    document.getElementById("message").innerHTML = fscore;
+    i = 0;
+    c = 0;
+    p = 0;
+  }
+};
